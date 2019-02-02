c7fc90df2688ef7c5f6d99e4a5fc4404973f5538
Goufix <alifer.17.11@gmail.com>
2019-02-01 22:45:58 -0200

    commit

--- src/HabboInfo.js
+++ src/HabboInfo.js
@@ -1,30 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import Result from './result'
+import Result from './Result';
+import Styles from './styles/styles.css';
 
 const API_ENDPOINT = 'https://www.habbo.com.br/api/public/users';
 
 export default function HabboInfo(props) {
   const [userdata, setUserdata] = useState(null);
 
-  useEffect(
-    async () => {
-      setUserdata(null);
+  useEffect(async () => {
+    setUserdata(null);
 
-      const response = await fetch(`${API_ENDPOINT}?name=${props.username}`);
-      const data = await response.json();
+    const response = await fetch(`${API_ENDPOINT}?name=${props.username}`);
+    const data = await response.json();
 
-      setUserdata(data);
-    },
-    [props.username]
-  );
+    setUserdata(data);
+  }, [props.username]);
 
   return (
     <div style={{ padding: '15px 0' }}>
       Dados para o usuário <code>{props.username}</code>:
       <pre style={{ marginTop: 15 }}>
-        {userdata === null
-          ? 'Carregando...'
-          : <Result data={userdata}> }
+        {userdata === null ? 'Carregando...' : <Result data={userdata} />}
       </pre>
     </div>
   );
--- src/result.js
+++ src/result.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Styles from './styles/styles.css';
 
 export default function Result(props) {
-  const {name, motto, uniqueId} = props.data
+  const { name, motto, uniqueId } = props.data;
 
-  return(
-    <table border="1">
-    <tr>
-      <td>{name}</td>
+  return (
+    <table className="resultTable" border="1">
+      <tr>
+        <td className="tableTitle">Nick</td>
+        <td className="tableTitle">Missão</td>
       </tr>
-  )
+      <tr>
+        <td>{name}</td>
+        <td>{motto}</td>
+      </tr>
+    </table>
+  );
 }
